783d6dd6f008b063483ffeb71852c7e55c82846b
Sourabh Singh <Sourabh@iiitmanipur.ac.in>
2022-07-16 19:02:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+import './App.css';
+import Hero from './components/Hero'
+import Topbar from './components/Topbar/topbar'
+
+function App() {
+  return (
+   <div className='Hero'>
+    <Topbar/>
+    <Hero/>
+
+   </div>
+
+  );
+}
+
+export default App;
---
+++ src/components/Topbar/topbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './topbar.css'
+
+function topbar() {
+  return (
+    <div className='topbar'>
+      <div className='left'>
+         PorTFo.
+      </div>
+      <div className='right'>
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default topbar
